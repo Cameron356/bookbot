0269213b07df777f811136cbe168f2c5540aa041
Cameron356 <CManicone@Proton.Me>
2024-07-03 11:20:37 -0400

    Chapter 11: dict parsed into a list of dicts, still need to sort alpha

--- main.py
+++ main.py
@@ -3,27 +3,38 @@ def main():
     book_path = 'books/frankenstein.txt'
     text = get_book_text(book_path) 
     num_words = get_num_words(text)
-    print(f"{num_words} words found in this document.")
-    letter_count = {}
-    lowered_text = text.lower()
-    just_letters = []
-    for char in lowered_text:
-        just_letters.append(char)
-    
-#def letter_counter(just_letters):
-    for letter in just_letters:
+    letter_dict = alpha_char_dict(text)
+    list_of_dict = dict_to_list(letter_dict)
+    print(list_of_dict)
+
+
+def dict_to_list(letter_dict):
+    dict_list = []
+    for char, num in letter_dict.items():
+        dict_list.append({char: num})
+    dict_list.sort()
+    return dict_list
+
+
+def alpha_char_dict(text):    
+    char = {}
+    for letter in text:
+        lowered = letter.lower()
         if letter.isalpha():
-            if letter in letter_count:
-                letter_count[letter] += 1
-            else: 
-                letter_count[letter] = 1
-    print(letter_count)
+            if lowered in char:
+                char[lowered] += 1
+            else:
+                char[lowered] = 1
+    return char
 
-
-
-
-
-
+##def letter_counter(just_letters):
+#    for letter in just_letters:
+#        if letter.isalpha():
+#            if letter in letter_count:
+#                letter_count[letter] += 1
+#            else: 
+#                letter_count[letter] = 1
+#    print(letter_count)
 
 def get_num_words(text):
     words = text.split()
