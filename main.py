78fa36e85a87c139ab2aca3caa340a1824b55ec6
Cameron356 <CManicone@Proton.Me>
2024-07-03 12:37:15 -0400

    Chapter 11: all parsed and sorted, printing report needs work

--- main.py
+++ main.py
@@ -4,15 +4,21 @@ def main():
     text = get_book_text(book_path) 
     num_words = get_num_words(text)
     letter_dict = alpha_char_dict(text)
-    list_of_dict = dict_to_list(letter_dict)
-    print(list_of_dict)
+    sorted_dict = sort_reverse(letter_dict)
+    list_of_dict = dict_to_list(sorted_dict)
+    print(f"the {list_of_dict[0].keys()} character was found {list_of_dict[0].values} times")
 
+
+
+
+def sort_reverse(letter_dict):
+    sort = (dict(sorted(letter_dict.items(), key = lambda x:x[1], reverse = True)))
+    return sort
 
 def dict_to_list(letter_dict):
     dict_list = []
     for char, num in letter_dict.items():
         dict_list.append({char: num})
-    dict_list.sort()
     return dict_list
 
 
@@ -27,14 +33,8 @@ def alpha_char_dict(text):
                 char[lowered] = 1
     return char
 
-##def letter_counter(just_letters):
-#    for letter in just_letters:
-#        if letter.isalpha():
-#            if letter in letter_count:
-#                letter_count[letter] += 1
-#            else: 
-#                letter_count[letter] = 1
-#    print(letter_count)
+
+
 
 def get_num_words(text):
     words = text.split()
